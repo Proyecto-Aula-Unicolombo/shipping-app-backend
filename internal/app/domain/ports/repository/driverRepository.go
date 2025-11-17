5b77cf8a06cf72fe5fc54ef6eba360b361f9afd3
Juan Barraza <juanbarraza2004@outlook.com>
2025-11-17 11:18:59 -0500

    add module drivers

--- internal/app/domain/ports/repository/driverRepository.go
+++ internal/app/domain/ports/repository/driverRepository.go
@@ -9,8 +9,9 @@ import (
 type DriverRepository interface {
 	CreateDriverTx(tx *sql.Tx, driver *entities.Driver) error
 	UpdateDriverTx(tx *sql.Tx, driver *entities.Driver) error
-	ListDrivers() ([]*entities.Driver, error)
+	ListDrivers(limit, offset int, nameLastNameOrNumOrder string) ([]*entities.Driver, error)
 	GetDriverByUserID(userID uint) (*entities.Driver, error)
 	DeleteDriverByUserIDTx(tx *sql.Tx, userID uint) error
 	GetByID(ctx context.Context, id uint) (*entities.Driver, error)
+	CountDrivers(nameLastNameOrNumOrder string) (int64, error)
 }
